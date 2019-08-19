30b5b6af1d835adda2bb170e93e3822c466277a1
Kurt Tamulonis <kurttamulonis@gmail.com>
2019-08-19 17:32:04 -0400

    initial commit

---
+++ db/schema.rb
@@ -0,0 +1,29 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2019_08_19_212612) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "btree_gist"
+  enable_extension "plpgsql"
+  enable_extension "uuid-ossp"
+
+  create_table "api_clients", force: :cascade do |t|
+    t.string "name", null: false
+    t.uuid "key", default: -> { "uuid_generate_v4()" }, null: false
+    t.boolean "enabled", default: true, null: false
+    t.datetime "created_at", default: -> { "now()" }, null: false
+    t.index ["key"], name: "index_api_clients_on_key", unique: true
+    t.index ["name"], name: "index_api_clients_on_name"
+  end
+
+end
